29357d3e04aab2ecec25182c06a90881749f4bf3
lotus <lotus920510@gmail.com>
2025-11-09 12:09:00 +0800

    v1.0.3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/army-life/',  // GitHub Pages 的 repository 名稱
   build: {
     outDir: 'docs'  // GitHub Pages 使用 docs 資料夾
   }
